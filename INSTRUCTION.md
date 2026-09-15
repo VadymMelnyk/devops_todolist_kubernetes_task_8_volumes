d3b71cc82d59e79d0e8674a07bf78b6f03d4c549
Vadym <vadym@example.com>
2026-09-15 19:50:38 +0300

    Solution2

--- INSTRUCTION.md
+++ INSTRUCTION.md
@@ -20,11 +20,11 @@ curl http://todoapp-service
 ### How to validate: ConfigMap data is mounted as files in the right order
 
 ```bash
-cat configs/PYTHONUNBUFFERED
+cat /app/configs/PYTHONUNBUFFERED
 ```
 
 ### How to validate: Secret data is mounted as a file
 
 ```bash
-cat secrets/SECRET_KEY
+cat /app/secrets/SECRET_KEY
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 kubectl apply -f .infrastructure/namespace.yml
 kubectl apply -f .infrastructure/clusterIp.yml
-kubectl apply -f .infrastructure/confgiMap.yml
+kubectl apply -f .infrastructure/configMap.yml
 kubectl apply -f .infrastructure/secret.yml
 kubectl apply -f .infrastructure/pv.yml
 kubectl apply -f .infrastructure/pvc.yml
